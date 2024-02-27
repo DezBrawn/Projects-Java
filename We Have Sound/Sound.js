f817850bbf5db2cb46cb5a7ace1a9492b77d17de
DezBrawn <115613445+DezBrawn@users.noreply.github.com>
2024-02-27 02:53:41 -0600

    Add files via upload

---
+++ We Have Sound/Sound.js
@@ -0,0 +1,23 @@
+/*
+
+    Dezerae Brawner
+    6-2-22
+    UAT Space Program - We Have SOUND!
+
+*/
+
+var audio = new Audio("514154__edwardszakal__game-music.mp3"); // variable = audio, with my link
+
+// function to play audio
+
+function playAudio()
+{
+    audio.play();
+}
+
+// function to pause audio
+
+function pauseAudio()
+{
+    audio.pause();
+}
